99a8469a8113c97ce29b545f34dd434dfef599b1
unknown <kamepalli.sudhir@gmail.com>
2023-07-10 06:13:27 +0530

    gateway implementation

--- order-service/src/main/java/com/sudhir/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sudhir/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.sudhir.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -15,6 +16,7 @@ public class OrderServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
